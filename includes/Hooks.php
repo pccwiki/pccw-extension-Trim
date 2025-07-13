f02de3ed52845c311984278531ec3878b7ef7158
pccwiki <admin@pocketcampcomplete.wiki>
2025-07-13 15:49:48 -0400

    Update Hooks.php

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -1,4 +1,38 @@
-public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
-	$out->addModules( 'ext.trimtag' );
-	return true;
+namespace TrimTag;
+
+use Parser;
+use PPFrame;
+use Html;
+use OutputPage;
+use Skin;
+
+class Hooks {
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setHook( 'trim', [ self::class, 'renderTrim' ] );
+		return true;
+	}
+
+	public static function renderTrim( $input, array $args, Parser $parser, PPFrame $frame ) {
+		$input = trim( $input );
+
+		if ( preg_match( '#^<p([^>]*)>(.*?)</p>$#si', $input, $matches ) ) {
+			$attrs = $matches[1];
+			$content = $matches[2];
+			if ( strpos( $attrs, 'data-trim=' ) === false ) {
+				$attrs .= ' data-trim="true"';
+			}
+			return "<p$attrs>$content</p>";
+		}
+
+		return Html::rawElement(
+			'p',
+			[ 'class' => 'trim-wrapper', 'data-trim' => 'true' ],
+			$input
+		);
+	}
+
+	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
+		$out->addModules( 'ext.trimtag' );
+		return true;
+	}
 }
